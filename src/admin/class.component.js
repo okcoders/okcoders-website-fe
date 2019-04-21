721739d658c68b21108edb4c31ce6a12cd86049b
Brayden Robbins <braydenrobbins98@gmail.com>
2019-04-21 14:50:42 -0500

    Changed from a list to a table for the class and started the modal to post new classes

--- src/admin/class.component.js
+++ src/admin/class.component.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import Config from '../config/app.local.conf.js'
-import { List } from 'antd'
-import { isEmpty } from 'lodash'
+import Config from '../config/app.local.conf';
+import { Table, Divider, Tag } from 'antd';
+import { isEmpty } from 'lodash';
+import CollectionsPage from './modalSubmit.component';
 
 export function Class(props) {
   const [classes, setClasses] = useState([]);
+  const { Column, CoumnGroup } = Table;
 
   useEffect(() => {
     if (isEmpty(classes)) {
@@ -17,23 +19,50 @@ export function Class(props) {
   return (
     <div>
       <h3>Previous Classes</h3>
-      <List
-        bordered
-        dataSource={classes}
-        renderItem={renderClass}
-      />
+      <Table dataSource={classes}>
+        <Column
+          title="Year Of Class"
+          dataIndex="yearOfClass"
+          key="yearOfClass"
+        />
+        <Column
+          title="Module Number"
+          dataIndex="moduleNumber"
+          key="moduleNumber"
+        />
+        <Column
+          title="Title"
+          dataIndex="title"
+          key="title"
+        />
+        <Column
+          title="Difficulty"
+          dataIndex="difficulty"
+          key="difficulty"
+        />
+        <Column
+          title="Languages"
+          dataIndex="languageTags"
+          key="languageTags"
+          render={tags => (
+            <span>
+              {tags.map(tag => <Tag color="blue" key={tag}>{tag}</Tag>)}
+            </span>
+          )}
+        />
+        <Column
+          title="Action"
+          key="action"
+          render={(text, record) => (
+            <span>
+              <a href="javascript:;">Edit {record.lastName}</a>
+              <Divider type="vertical" />
+              <a href="javascript:;">Delete</a>
+            </span>
+          )}
+        />
+      </Table>
+      <h3>Add New </h3>
     </div>
   );
-}
-
-function renderClass(classes) {
-  return (
-    <List.Item>
-      {classes.yearOfClass}
-      {classes.moduleNumber}
-      {classes.languageTags}
-      {classes.title}
-      {classes.difficulty}
-    </List.Item>
-  )
 }
