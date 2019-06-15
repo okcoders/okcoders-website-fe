ce6228124656edc46037f6b0ba7e0e1ead8e16d0
Brayden Robbins <braydenrobbins98@gmail.com>
2019-06-15 10:04:21 -0500

    cleaned up some logs and handled the query for the alumni fot all boolean options

--- src/admin/class.component.js
+++ src/admin/class.component.js
@@ -48,8 +48,6 @@ function Class() {
       })
   }
 
-
-
   function saveNewLanguage(e) {
     if (!newLanguage) {
       return
@@ -143,15 +141,7 @@ function Class() {
           <p>Github Profile: {selectedAlumni.github}</p>
           <p>Linked In Profile: {selectedAlumni.linkedin}</p>
           <p>Classes taken: {(selectedAlumni.languages || []).join(', ')}</p>
-          {/* 
-  email: String,
-  bio: String,
-  birthday: String,
-  linkedin: String,
-  github: String,
-  classes: [{type: Schema.Types.ObjectId, ref: 'Class' }], */}
         </Modal>
-        {/* MAKE THIS INTO A TABLE */}
         <h3 style={{ margin: '16px 0' }}>Confirm New Alumni</h3>
         <List
           bordered
